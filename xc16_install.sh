1f3b3f93a42aba0cd6ff3336d912ba17027abc85
Cameron Carslake <cscarslake@protonmail.com>
2023-03-18 21:19:04 +1030

    Fixed xc16 install options

--- xc16_install.sh
+++ xc16_install.sh
@@ -4,7 +4,7 @@ INSTALL_DIR="/opt"
 
 XC16_URL="https://ww1.microchip.com/downloads/aemDocuments/documents/DEV/ProductDocuments/SoftwareTools/xc16-v2.00-full-install-linux64-installer.run"
 XC16_INSTALLER="xc16_installer.run"
-OPTIONS=" --unattendedmodeui none --mode unattended --debuglevel 4 --LicenseType FreeMode --netservername localhost --Add to Path 1"
+OPTIONS=" --unattendedmodeui none --mode unattended --debuglevel 4 --LicenseType FreeMode --netservername localhost"
 
 wget -O "$INSTALL_DIR/$XC16_INSTALLER" $XC16_URL
 chmod u+x "$INSTALL_DIR/$XC16_INSTALLER"
