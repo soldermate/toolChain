9f4b202d3ab6131938908f7e20d74a3f8bbcb38c
cam <cscarslake@protonmail.com>
2024-01-08 21:10:36 +1030

    Fixed mplab install script

--- mplab_install.sh
+++ mplab_install.sh
@@ -2,11 +2,13 @@
 
 INSTALL_DIR="/opt"
 
-MPLAB_URL="https://ww1.microchip.com/downloads/aemDocuments/documents/DEV/ProductDocuments/SoftwareTools/MPLABX-v6.05-linux-installer.tar"
-SCRIPT="MPLABX-v6.05-linux-installer.sh"
+MPLAB_URL="https://ww1.microchip.com/downloads/aemDocuments/documents/DEV/ProductDocuments/SoftwareTools/MPLABX-v6.15-linux-installer.tar"
+MPLAB_REFERER="https://www.microchip.com/en-us/tools-resources/develop/mplab-x-ide#"
+SCRIPT="MPLABX-v6.15-linux-installer.sh"
 OPTIONS="-- --unattendedmodeui minimal --mode unattended --ide 1 --ipe 1 --8bitmcu 0 --16bitmcu 1 --32bitmcu 0 --othermcu 0 --collectInfo 0 --collectMyMicrochipInfo 0"
 
-wget -O "$INSTALL_DIR/mplab.tar" $MPLAB_URL
+# The link won't download without the referer option
+wget -O "$INSTALL_DIR/mplab.tar" --referer=$MPLAB_REFERER $MPLAB_URL
 tar -xvf "$INSTALL_DIR/mplab.tar" -C $INSTALL_DIR
 
 # Run MPLAB installer script
