cc72fb1ea3cc2cbb3a8aebf84e8134ca445ee883
cam <cscarslake@protonmail.com>
2024-01-15 20:04:04 +1030

    Added mdb debugger to path

--- mplab_install.sh
+++ mplab_install.sh
@@ -14,6 +14,13 @@ tar -xvf "$INSTALL_DIR/mplab.tar" -C $INSTALL_DIR
 # Run MPLAB installer script
 $INSTALL_DIR/$SCRIPT $OPTIONS
 
+# Add MDB debugger to PATH of the user who invoked the script
+MDB_PATH="$INSTALL_DIR/microchip/mplabx/v6.15/mplab_platform/bin"
+
+echo "Adding $MDB_PATH to the PATH of $SUDO_USER"
+su -c "echo -en "PATH=$PATH:$MDB_PATH" >> ~/.bash_profile" $SUDO_USER
+su -c "source ~/.bash_profile" $SUDO_USER
+
 # Delete MPLAB tar and install script
 rm -v "$INSTALL_DIR/mplab.tar"
 rm -v "$INSTALL_DIR/$SCRIPT"
