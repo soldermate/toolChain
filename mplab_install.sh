8ce2b571bdd629a8b86ee0491de409ea1297d5d5
cam <cscarslake@protonmail.com>
2024-03-11 17:53:10 +1030

    Added referer for web link download

--- mplab_install.sh
+++ mplab_install.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+# For some reason if microchip is already on the users path then this silly install will muck up the existing paths.
+# Need to manually correct the much up until a work around.
+
 INSTALL_DIR="/opt"
 
-MPLAB_URL="https://ww1.microchip.com/downloads/aemDocuments/documents/DEV/ProductDocuments/SoftwareTools/MPLABX-v6.15-linux-installer.tar"
-MPLAB_REFERER="https://www.microchip.com/en-us/tools-resources/develop/mplab-x-ide#"
+URL="https://ww1.microchip.com/downloads/aemDocuments/documents/DEV/ProductDocuments/SoftwareTools/MPLABX-v6.15-linux-installer.tar"
+REFERER="https://www.microchip.com/en-us/tools-resources/develop/mplab-x-ide#"
 SCRIPT="MPLABX-v6.15-linux-installer.sh"
 OPTIONS="-- --unattendedmodeui minimal --mode unattended --ide 1 --ipe 1 --8bitmcu 0 --16bitmcu 1 --32bitmcu 0 --othermcu 0 --collectInfo 0 --collectMyMicrochipInfo 0"
 
 # The link won't download without the referer option
-wget -O "$INSTALL_DIR/mplab.tar" --referer=$MPLAB_REFERER $MPLAB_URL
+wget -O "$INSTALL_DIR/mplab.tar" --referer=$REFERER $URL
 tar -xvf "$INSTALL_DIR/mplab.tar" -C $INSTALL_DIR
 
 # Run MPLAB installer script
